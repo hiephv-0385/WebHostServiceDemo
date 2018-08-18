a3f9d66ab88d223ac6d0309fbd7a2ebcccc980ed
Han Van Hiep <quanghiepth86@gmail.com>
2018-08-18 12:32:15 +0700

    Init project

---
+++ WebHostServiceDemo/CustomWebHostService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebHostServiceDemo
+{
+    public class CustomWebHostService: WebHostService
+    {
+        private ILogger _logger;
+
+        public CustomWebHostService(IWebHost host) : base(host)
+        {
+            _logger = host.Services.GetRequiredService<ILogger<CustomWebHostService>>();
+        }
+
+        protected override void OnStarting(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch();
+            base.OnStarting(args);
+        }
+
+        protected override void OnStarted()
+        {
+            base.OnStarted();
+        }
+
+        protected override void OnStopping()
+        {
+            base.OnStopping();
+        }
+    }
+}
---
+++ WebHostServiceDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
+
+namespace WebHostServiceDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+            IWebHost host;
+
+            if (Debugger.IsAttached || args.Contains("console"))
+            {
+                host = WebHost.CreateDefaultBuilder()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseStartup<Startup>()
+                        .Build();
+                host.Run();
+            }
+            else
+            {
+                host = WebHost.CreateDefaultBuilder(args)
+                        .UseContentRoot(pathToContentRoot)
+                        .UseStartup<Startup>()
+                        .Build();
+                host.RunAsCustomService();
+            }
+        }
+    }
+}
